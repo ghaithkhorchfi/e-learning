6783d97274089175b130a1e262bf1085a3855311
Med Selim Ferroukhi <83102012+Selim9999@users.noreply.github.com>
2022-04-23 04:15:51 +0100

    third commit

--- src/app/component/profil/profil.component.ts
+++ src/app/component/profil/profil.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent implements OnInit {
+  loggedInUser: any = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.getLoggedInUser();
+  }
+
+  getLoggedInUser() {
+    this.loggedInUser = JSON.parse(localStorage.getItem("user"));
+    console.log(this.loggedInUser);
+    console.log("3asba")
   }
 
 }
